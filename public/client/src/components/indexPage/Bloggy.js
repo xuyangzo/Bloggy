c9fade49cb1a83fb968295baf8a9b86c287c0f54
Lynch Zou <lynch0114@outlook.com>
2019-01-17 18:54:37 -0800

    Add necessary dependencies

--- public/client/src/components/indexPage/Bloggy.js
+++ public/client/src/components/indexPage/Bloggy.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import Header from './Header';
-import Body from './Body';
-import Form from './Form.test';
+import React from "react";
+import Header from "./Header";
+import Body from "./Body";
+import Form from "./Form.test";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default class Bloggy extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                <Header />
-                <Body />
-                <Form />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Body />
+        <Form />
+      </div>
+    );
+  }
 }
