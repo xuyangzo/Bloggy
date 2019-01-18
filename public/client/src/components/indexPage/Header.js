b9f623e5c6f6b669995ec2339a6f609b8a6cf2aa
zhixul <zhixul@usc.edu>
2019-01-17 23:13:00 -0800

    qiangxing commit

--- public/client/src/components/indexPage/Header.js
+++ public/client/src/components/indexPage/Header.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header Test</h1>
-        </div>
+        
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Bloggy</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                
+                <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+       
     );
 }
 
