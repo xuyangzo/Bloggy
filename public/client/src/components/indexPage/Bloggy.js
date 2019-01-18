89ff00c6ef93a82a2162210b782ac95c1bfd196c
Lynch Zou <lynch0114@outlook.com>
2019-01-17 23:03:10 -0800

    Add connect between React and Node

--- public/client/src/components/indexPage/Bloggy.js
+++ public/client/src/components/indexPage/Bloggy.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Header from "./Header";
+import Navbar from "../common/Navbar";
 import Body from "./Body";
-import Form from "./Form.test";
-import { BrowserRouter as Router } from "react-router-dom";
+import Register from "../auth/Register";
+import Login from "../auth/Login";
 
 export default class Bloggy extends React.Component {
   constructor(props) {
@@ -11,11 +14,17 @@ export default class Bloggy extends React.Component {
 
   render() {
     return (
-      <div>
-        <Header />
-        <Body />
-        <Form />
-      </div>
+      <Router>
+        <div>
+          <Header />
+          <Navbar />
+          <Route exact path="/" component={Body} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
