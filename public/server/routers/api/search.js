a223c9892f7c786adfecc321b234889f768d7cfc
Hao Chen <chen579@usc.edu>
2019-01-22 23:01:27 -0800

    api-search-title

--- public/server/routers/api/search.js
+++ public/server/routers/api/search.js
@@ -5,7 +5,7 @@ const Post = require("../../models/Post");
 // @route   GET api/posts/view
 // @desc    View Post
 // @access  Public
-router.get("/view/:author", (req, res) => {
+router.get("/view/author/:author", (req, res) => {
     let errors = {};
     Post.find({author: req.params.author})
         .then(post => {
@@ -21,3 +21,20 @@ router.get("/view/:author", (req, res) => {
         );
 });
 
+router.get("/view/title/:title", (req, res) => {
+    let errors = {};
+    Post.find({title: req.params.title})
+        .then(post => {
+            if (!post) {
+                errors.post = "There is no content for this post";
+                res.status(404).json(errors);
+            }
+
+            res.json(post);
+        })
+        .catch(err =>
+            res.status(404).json({ post: "There is no content fot this post" })
+        );
+});
+
+module.exports = router;
