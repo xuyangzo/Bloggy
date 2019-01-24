91471c4a97073c83401e65366a6a94bc73dcce6b
Jackie Dong <dongjackie1999@gmail.com>
2019-01-24 15:44:46 -0800

    loaded bootstrap

--- public/client/src/app.js
+++ public/client/src/app.js
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../html/css/clean-blog.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Bloggy from './components/indexPage/Bloggy';
