0195f46d0a31761fdf7f9a171af64712f2f03ffa
Jackie Dong <dongjackie1999@gmail.com>
2019-01-24 16:33:41 -0800

    updated all routes

--- public/client/src/components/aboutPage/Header.js
+++ public/client/src/components/aboutPage/Header.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <header class="masthead" style={{ backgroundImage: "url('./html/img/home-bg.jpg')" }}>
-            <div class="overlay"></div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-md-10 mx-auto">
-                        <div class="site-heading">
-                            <h1>Bloggy</h1>
-                            <span class="subheading">Create your blog today</span>
-                            <div class="col-lg-6 col-md-7 mx-auto text-center mt-4">
-                                <Link className="nav-link" to="/register">
-                                    <button type="button" class="btn btn-outline-light btn-block">Sign Up</button>
-                                </Link>
-                                <Link className="nav-link" to="/login">
-                                    <button type="button" class="btn btn-outline-light btn-block">Login</button>
-                                </Link>
-                            </div>
+        <header className="masthead" style={{backgroundImage: "url('../../../html/img/about-bg.jpg')"}}>
+            <div className="overlay"></div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 col-md-10 mx-auto">
+                        <div className="page-heading">
+                            <h1>About Me</h1>
+                            <span className="subheading">This is what I do.</span>
                         </div>
                     </div>
                 </div>
             </div>
-        </header >
+        </header>
     );
 }
 
