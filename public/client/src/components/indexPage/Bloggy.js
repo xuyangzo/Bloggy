56fc50a03e6d4610cc2b63fde2b635000d1f9bf1
Lynch Zou <lynch0114@outlook.com>
2019-01-27 13:27:49 -0800

    Login/Register update, Validation Change

--- public/client/src/components/indexPage/Bloggy.js
+++ public/client/src/components/indexPage/Bloggy.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Common
 import Navbar from "../common/Navbar";
 import Register from "../auth/Register";
 import Login from "../auth/Login";
+import NotFound from "../common/NotFound";
 import Footer from "../common/Footer";
 
 // Index Page
@@ -29,12 +30,15 @@ export default class Bloggy extends React.Component {
       <Router>
         <div>
           <Navbar />
-          <Route exact path="/" component={Index} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/post" component={Post} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
+          <Switch>
+            <Route exact path="/" component={Index} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/post" component={Post} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route component={NotFound} />
+          </Switch>
           <Footer />
         </div>
       </Router>
