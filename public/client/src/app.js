c32ec31c0c1363afeb6315c3cae4b79bc0d850b7
zhixul <zhixul@usc.edu>
2019-01-27 14:22:37 -0800

    delete template

--- public/client/src/app.js
+++ public/client/src/app.js
@@ -1,9 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../html/css/clean-blog.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Bloggy from './components/indexPage/Bloggy';
-import '../../html/scss/clean-blog.scss';
 
 ReactDOM.render(<Bloggy />, document.getElementById('app'));
