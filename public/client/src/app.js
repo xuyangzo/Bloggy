f0c54f1b7c114f1a4eef14460bd33deb103810af
xuyangzo <xuyangzo@usc.edu>
2019-01-27 13:27:49 -0800

    Login/Register update, Validation Change

--- public/client/src/app.js
+++ public/client/src/app.js
@@ -1,9 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../html/css/clean-blog.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Bloggy from './components/indexPage/Bloggy';
-import '../../html/scss/clean-blog.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../html/css/clean-blog.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import React from "react";
+import ReactDOM from "react-dom";
+import Bloggy from "./components/indexPage/Bloggy";
+import "../../html/scss/clean-blog.scss";
+import "../src/styles/style.scss";
 
-ReactDOM.render(<Bloggy />, document.getElementById('app'));
+ReactDOM.render(<Bloggy />, document.getElementById("app"));
