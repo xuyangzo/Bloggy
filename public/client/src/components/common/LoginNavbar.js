9b739bdcadfffeee4ecee4dff5055483e045c1c5
Lynch Zou <lynch0114@outlook.com>
2019-02-04 18:30:28 -0800

    Index Page Headroom

--- public/client/src/components/common/LoginNavbar.js
+++ public/client/src/components/common/LoginNavbar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
+import Headroom from "react-headroom";
 
 class LoginNavbar extends React.Component {
   constructor(props) {
@@ -40,52 +41,54 @@ class LoginNavbar extends React.Component {
 
   render() {
     return (
-      <nav
-        className="navbar navbar-expand-md navbar-light fixed-top navbar-light bg-light"
-        id="mainNav"
-      >
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            Bloggy
-          </Link>
-          <button
-            className="navbar-toggler navbar-toggler-right"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
+      <Headroom>
+        <nav
+          className="navbar navbar-expand-md navbar-light bg-light"
+          id="mainNav"
+        >
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              Bloggy
+            </Link>
+            <button
+              className="navbar-toggler navbar-toggler-right"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon" />
+            </button>
 
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <div class="ml-auto">
-              <SearchBar />
-            </div>
-            <ul className="navbar-nav ml-auto">
-              {/* <li className="nav-item mr-auto">
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <div class="ml-auto">
+                <SearchBar />
+              </div>
+              <ul className="navbar-nav ml-auto">
+                {/* <li className="nav-item mr-auto">
                 
               </li> */}
-              <li className="nav-item">
-                <img
-                  className="profile-img mt-1"
-                  alt="profile picture"
-                  src={this.state.avatar}
-                  onClick={this.onImgClick}
-                />
-              </li>
+                <li className="nav-item">
+                  <img
+                    className="profile-img mt-1"
+                    alt="profile picture"
+                    src={this.state.avatar}
+                    onClick={this.onImgClick}
+                  />
+                </li>
 
-              <li className="nav-item">
-                <a className="nav-link" href="#" onClick={this.onClick}>
-                  Log Out
-              </a>
-              </li>
-            </ul>
+                <li className="nav-item">
+                  <a className="nav-link" href="#" onClick={this.onClick}>
+                    Log Out
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </Headroom>
     );
   }
 }
