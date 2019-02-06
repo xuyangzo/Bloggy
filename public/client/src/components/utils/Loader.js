48c1a731b563b6c95b07c36a1c8b7275827465ad
Lynch Zou <lynch0114@outlook.com>
2019-02-06 14:07:45 -0800

    Edit, Middle of Page Bug Fix

---
+++ public/client/src/components/utils/Loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loader = () => (
+  <div class="lds-roller">
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
