c52a8cc6a28bb16a6622c9138901db0cdc1daae1
zhixul <zhixul@usc.edu>
2019-02-08 14:20:04 -0800

    shuai

---
+++ public/client/src/components/searchPage/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../utils/setAuthToken";
+import Unauthorized from "../utils/Unauthorized";
+import Navbar from "../common/Navbar";
+
+export default class Search extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+          <div>
+            zuwazidongxi
+          </div>
+        )
+    }
+}
