2f18486f033e954d306f01998b8ca04fe004267e
Hao Chen <chen579@usc.edu>
2019-02-13 00:59:56 -0800

    redis-subscribe

--- public/client/src/components/common/Navbar.js
+++ public/client/src/components/common/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Headroom from "react-headroom";
 import SearchBar from "./SearchBar";
+
 export default class Navbar extends React.Component {
   constructor(props) {
     super(props);
