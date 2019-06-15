be2e3749291603939870e189b4096698d60058d3
zhixul <zhixul@usc.edu>
2019-06-14 19:57:10 -0700

    fix bug

--- public/client/src/components/profile/Profile.js
+++ public/client/src/components/profile/Profile.js
@@ -6,7 +6,7 @@ import setAuthToken from "../utils/setAuthToken";
 import Unauthorized from "../utils/Unauthorized";
 
 import ProfileContent from "./ProfileContent";
-import Follow from "./Follow"
+
 export default class Profile extends React.Component {
   constructor(props) {
     super(props);
@@ -24,8 +24,10 @@ export default class Profile extends React.Component {
     return (
       <div class="container my-5" style={{ paddingTop: "65px" }}>
         <div class="row justify-content-md-center">
+        
           <div class="col-sm-10 col-md-12 mx-auto">
             <Favorite />
+            
             <ProfileContent
               onClickPost={this.onClickPost}
               userid={this.props.match.params.userid}
